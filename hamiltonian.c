e09c199df1f6ffa18e9ad35e00ddc9584027cd64
Mike Schaeffer <mike@mschaef.com>
2020-08-30 22:20:05 -0400

    Clean up variable names in search algorithm.

--- hamiltonian.c
+++ hamiltonian.c
@@ -6,8 +6,8 @@
 
 #define MAX_NODES 100
 
-int path_visited_p(size_t lim, graph_node buf[], graph_node n) {
-     for(size_t ii = 0; ii < lim; ii++) {
+int path_visited_p(graph_node buf[], size_t path_len, graph_node n) {
+     for(size_t ii = 0; ii < path_len; ii++) {
           if (buf[ii] == n) {
                return TRUE;
           }
@@ -16,7 +16,7 @@ int path_visited_p(size_t lim, graph_node buf[], graph_node n) {
      return FALSE;
 }
 
-int find_hamiltonian_path1(struct graph *g, size_t lim, graph_node buf[], size_t ofs, graph_node at) {
+int find_hamiltonian1(struct graph *g, size_t lim, graph_node path_buf[], size_t ofs, graph_node at) {
 
      if (ofs >= lim) {
           return -1;
@@ -33,13 +33,13 @@ int find_hamiltonian_path1(struct graph *g, size_t lim, graph_node buf[], size_t
      for(int ii = 0; ii < count; ii++) {
           graph_node next = nodes[ii];
 
-          if (path_visited_p(ofs, buf, next)) {
+          if (path_visited_p(path_buf, ofs, next)) {
                continue;
           }
 
-          buf[ofs] = next;
+          path_buf[ofs] = next;
 
-          int path_len = find_hamiltonian_path1(g, lim, buf, ofs + 1, next);
+          int path_len = find_hamiltonian1(g, lim, path_buf, ofs + 1, next);
 
           if (path_len >= 0) {
                return path_len;
@@ -49,15 +49,15 @@ int find_hamiltonian_path1(struct graph *g, size_t lim, graph_node buf[], size_t
      return -1;
 }
 
-int find_hamiltonian_path(struct graph *g, size_t lim, graph_node buf[]) {
+int find_hamiltonian(struct graph *g, size_t lim, graph_node path_buf[]) {
      graph_node nodes[MAX_NODES];
 
      int count = graph_get_all_nodes(g, MAX_NODES, nodes);
 
      for(int ii = 0; ii < count; ii++) {
-          buf[0] = nodes[ii];
+          path_buf[0] = nodes[ii];
 
-          int path_len = find_hamiltonian_path1(g, lim, buf, 1, buf[0]);
+          int path_len = find_hamiltonian1(g, lim, path_buf, 1, path_buf[0]);
 
           if (path_len >= 0) {
                return path_len;
@@ -81,7 +81,7 @@ int main(int argc, char *argv[]) {
 
      graph_node nodes[MAX_NODES];
 
-     int count = find_hamiltonian_path(g1, MAX_NODES, nodes);
+     int count = find_hamiltonian(g1, MAX_NODES, nodes);
 
      if (count < 0) {
           printf("Hamiltonian path not found.\n");
